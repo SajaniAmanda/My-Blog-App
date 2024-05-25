55b5ebbad0ec1dc8d1b2a9e9c16fed1da179ba57
W.S.Amanda <e19017@eng.pdn.ac.lk>
2024-05-25 19:55:45 +0530

    Developed frontend and backend

---
+++ BlogApp/src/main/java/com/example/BlogApp/service/PostService.java
@@ -0,0 +1,10 @@
+package com.example.BlogApp.service;
+
+import com.example.BlogApp.entity.Post;
+
+public interface PostService {
+
+    Post savePost(Post post);
+
+
+}
---
+++ BlogApp/src/main/java/com/example/BlogApp/service/PostServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.BlogApp.service;
+
+import com.example.BlogApp.entity.Post;
+import com.example.BlogApp.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class PostServiceImpl implements PostService{
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post savePost(Post post){
+        post.setLikeCount(0);
+        post.setViewCount(0);
+        post.setDate(new Date());
+
+        return postRepository.save(post);
+    }
+
+}
